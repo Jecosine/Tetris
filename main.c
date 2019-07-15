f4ea21e0f39c916faf7f5a7c0961a40e3e4e8881
silver <mj_ol@outlook.com>
2019-07-15 10:20:04 +0900

    INIT:Init repo

---
+++ main.c
@@ -0,0 +1,9 @@
+#include "tetris.h"
+
+
+
+int main()
+{
+    printf("Hello world!\n");
+    return 0;
+}
---
+++ tetris.h
@@ -0,0 +1,16 @@
+#ifndef TETRIS_H_INCLUDED
+#define TETRIS_H_INCLUDED
+#endif // TETRIS_H_INCLUDED
+#include <stdlib.h>
+#include <stdio.h>
+// define game element
+#define up 1
+#define right 2
+#define down 3
+#define left 4
+//function declare
+void locate(COORD pos);
+void color(int c);
+void
+
+
