51cacb3572656bf46a8b3317b5319f0843ddeb4c
silver <mj_ol@outlook.com>
2019-07-15 10:45:07 +0900

    UPDATE

--- tetris.h
+++ tetris.h
@@ -3,14 +3,33 @@
 #endif // TETRIS_H_INCLUDED
 #include <stdlib.h>
 #include <stdio.h>
+#include <windows.h>
+#include <time.h>
+#include <random>
 // define game element
 #define up 1
 #define right 2
 #define down 3
 #define left 4
+#define DEFAULT_W 100
+#define DEFAULT_H 40
 //function declare
 void locate(COORD pos);
 void color(int c);
-void
+void cleanScreen();
+char shuffleShape();
+int loadShapes();
+int checkBottom();
+int checkTop();
+int[][] initCanvas();
+
+
+//define Tetromino
+//Shape type S Z L J I O T
+
+struct shape{
+    COORD pos;
+    char t;
+};
 
 
