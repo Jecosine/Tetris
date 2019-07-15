5da7d02431989e0f3ce6126bbaf306a42a000138
silver <mj_ol@outlook.com>
2019-07-15 12:22:42 +0900

    UPDATE:Update Pos

--- main.c
+++ main.c
@@ -25,14 +25,61 @@
 //¡ö¡ö¡ö
 //Color settings
 //I:03  J:02    L:06    O:0E    S:0A    Z:0C    T:05
+//I = (0,-1),(0,0),(0,1),(0,2)
+//J = (-1,-1),(-1,0),(0,0),(1,0)
+//L = (-1,0),(0,0),(1,0),(1,1)
+//O = (0,0),(0,1),(1,0),(0,1)
+//S = (-1,1),(0,1),(0,0),(0,1)
+//Z = (-1,0),(0,0),(0,1),(1,1)
+//T = (-1,0),(0,0),(0,1),(1,0)
 void setColor(int c){
     SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE),c);
 }
+void clearScreen(){
+    system("cls");
+}
+void setConsoleSize(int x, int y){
+    HANDLE h = GetStdHandle(STD_OUTPUT_HANDLE);
+    COORD size = {x, y};
+    SMALL_RECT rc = {0,0,size.X - 1,size.Y - 1};
+    //hide cursor
+    CONSOLE_CURSOR_INFO cur = {1, FALSE};
+    SetConsoleWindowInfo(h, TRUE, &rc);
+    SetConsoleScreenBufferSize(h, size);
+    SetConsoleCursorInfo(h, &cur);
+
+}
+int* initCanvas(int width,int height){
+    int canvas[height][width];
+    for(int i = 0;i < height;i++){
+        for(int j = 0;j<width;j++){
+            canvas[i][j] = 0;
+        }
+    }
+}
+struct Tetromino* genShapes(int t){
+    switch(t){
+    case 0:
+        int poses[4][2] = {(0,-1),(0,0),(0,1),(0,2)};
+    case 1:
+        int poses[4][2] = {(-1,-1),(-1,0),(0,0),(1,0)};
+    case 2:
+        int poses[4][2] = {(-1,0),(0,0),(1,0),(1,1)};
+    case 3:
+        int poses[4][2] = {(0,0),(0,1),(1,0),(0,1)};
+    case 4:
+        int poses[4][2] = {(-1,1),(0,1),(0,0),(0,1)};
+    case 5:
+        int poses[4][2] = {(-1,0),(0,0),(0,1),(1,1)};
+    case 6:
+        int poses[4][2] = {(-1,0),(0,0),(0,1),(1,0)};
+
+    }
+}
 int main()
 {
-    for(int i = 0;i < 50;i++){
-        setColor(i);
-        printf("%d -> ¡ö\n",i);
-    }
+    int poses[4][2] = {{0,0},{},{},{}};
+    free(poses);
+    printf("%d,%d",poses[0][0],poses[0][1]);
     return 0;
 }
--- tetris.h
+++ tetris.h
@@ -7,10 +7,10 @@
 #include <time.h>
 
 // define game element
-#define up 1
-#define right 2
-#define down 3
-#define left 4
+#define upward 1
+#define rightward 2
+#define downward 3
+#define leftward 4
 #define DEFAULT_W 100
 #define DEFAULT_H 40
 //Shapes
@@ -26,9 +26,12 @@ void locate(COORD pos);
 void setColor(int c);
 void cleanScreen();
 void placeShape();
-void renderShape();
+void renderShape(struct Tetromino* t);
+void rotateShape(struct Tetromino* t);
+void setConsoleSize(int x, int y);
+void drop(struct Tetromino *t);
 char shuffleShape();
-int loadShapes();
+struct Tetromino* genShapes(int t);
 int checkBottom();
 int checkTop();
 int* initCanvas();
@@ -41,7 +44,11 @@ struct pixel{
     struct pixel *next;
 };
 struct Tetromino{
+    COORD pos;
+    int left;
+    int right;
     struct pixel *head;
+    struct pixel *center;
     struct pixel *tail;
 };
 
